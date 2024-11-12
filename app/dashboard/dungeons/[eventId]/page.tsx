cf4b39a725a6625aa2e7b3d94bf7716481d1b18f
vinicius <vinisaio10@gmail.com>
2024-11-12 12:19:40 -0300

    fix padding bot

--- app/dashboard/dungeons/[eventId]/page.tsx
+++ app/dashboard/dungeons/[eventId]/page.tsx
@@ -280,7 +280,7 @@ export default function Dungeons() {
           </AlertDialog>
           </div>
         </div>
-        <div className="flex flex-col justify-center lg:flex-row gap-4">
+        <div className="flex flex-col justify-center lg:flex-row gap-4 mb-8">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {roles.map((role) => renderPlayerSlot(role))}
           </div>
